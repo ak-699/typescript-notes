07397c528251e2f96a003df0486a9584e47d77a9
Abhishek Kumar <kumar699.abhishek@gmail.com>
2024-09-05 23:14:58 +0530

    Add interface and type to arguments and variables

--- why-typescript/app.ts
+++ why-typescript/app.ts
@@ -1,4 +1,29 @@
-function getProduct(id) {
+// function getProduct(id) {
+//   return {
+//     id,
+//     name: `Awesome gadget ${id}`,
+//     price: 99,
+//   };
+// }
+// const product = getProduct(1);
+
+// in this the product.Name property exists
+// console.log(`The product ${product.Name} costs: ${product.price}`);
+
+// const showProduct = (name, price) => {
+//   console.log(`Product name: ${name} and price: ${price}`);
+// };
+
+// js dosent care that we misplaced the arguments
+// showProduct(product.price, product.name);
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
+function getProduct(id: number): Product {
   return {
     id,
     name: `Awesome gadget ${id}`,
@@ -6,13 +31,9 @@ function getProduct(id) {
   };
 }
 const product = getProduct(1);
-
 // in this the product.Name property exists
-// console.log(`The product ${product.Name} costs: ${product.price}`);
+console.log(`The product ${product.name} costs: ${product.price}`);
 
-const showProduct = (name, price) => {
+const showProduct = (name: string, price: number) => {
   console.log(`Product name: ${name} and price: ${price}`);
 };
-
-// js dosent care that we misplaced the arguments
-// showProduct(product.price, product.name);
