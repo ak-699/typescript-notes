b817fad731256d4588c0336caa3e865799d0c053
Abhishek Kumar <kumar699.abhishek@gmail.com>
2024-09-05 23:05:11 +0530

    Add wrong type of values and different order arguements in why-transcript

---
+++ why-typescript/app.ts
@@ -0,0 +1,18 @@
+function getProduct(id) {
+  return {
+    id,
+    name: `Awesome gadget ${id}`,
+    price: 99,
+  };
+}
+const product = getProduct(1);
+
+// in this the product.Name property exists
+// console.log(`The product ${product.Name} costs: ${product.price}`);
+
+const showProduct = (name, price) => {
+  console.log(`Product name: ${name} and price: ${price}`);
+};
+
+// js dosent care that we misplaced the arguments
+// showProduct(product.price, product.name);
