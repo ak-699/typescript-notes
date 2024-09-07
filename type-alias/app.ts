363f1761d70f0973ac56c79af0dbf434a2790daa
Abhishek Kumar <kumar699.abhishek@gmail.com>
2024-09-07 23:19:58 +0530

    Type alias for objects

--- type-alias/app.ts
+++ type-alias/app.ts
@@ -1,7 +1,23 @@
 // type alias: existingType
-type Name = String;
-let firstName: Name;
-let lastName: Name;
+// type Name = String;
+// let firstName: Name;
+// let lastName: Name;
 
-firstName = "abhishek";
-console.log(firstName);
+// firstName = "abhishek";
+// console.log(firstName);
+
+type Person = {
+  name: string;
+  age: number;
+};
+
+// if i keep person as vairable name i get below error
+// Cannot redeclare block-scoped variable 'person'.ts(2451)
+// app.ts(15, 5): 'person' was also declared here.
+
+let person1: Person = {
+  name: "abhishek",
+  age: 21,
+};
+
+console.log(person1);
