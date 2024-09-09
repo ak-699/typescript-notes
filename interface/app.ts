44849368d9afa6b7aa9edfd79ecbd7fc6c3d3a14
Abhishek Kumar <kumar699.abhishek@gmail.com>
2024-09-09 22:18:36 +0530

    Interface with optional properties

--- interface/app.ts
+++ interface/app.ts
@@ -4,18 +4,32 @@
 
 // greetUser({ name: "abhishek", age: 21 });
 
-namespace PersonNamespace {
-  export interface Person {
-    firstName: string;
-    lastName: string;
-  }
+// namespace PersonNamespace {
+//   export interface Person {
+//     firstName: string;
+//     lastName: string;
+//   }
+// }
+
+// function greetUser(person: PersonNamespace.Person) {
+//   console.log(person);
+// }
+// let user: PersonNamespace.Person = {
+//   firstName: "abhishek",
+//   lastName: "kumar",
+// };
+// greetUser(user);
+
+// Optional Properties
+// To make a property optional in a object put a question mark
+interface Dog {
+  firstName: String;
+  lastName: String;
+  middleName?: string;
 }
 
-function greetUser(person: PersonNamespace.Person) {
-  console.log(person);
+function getFullName(dog: Dog) {
+  console.log(dog);
 }
-let user: PersonNamespace.Person = {
-  firstName: "abhishek",
-  lastName: "kumar",
-};
-greetUser(user);
+
+getFullName({ firstName: "Tony", lastName: "Stark", middleName: "fucking" });
